0bbacc74aae1f0e5c47d36c8c94762444f96c164
Shdjgfnkg <93859977+Shdjgfnkg@users.noreply.github.com>
2023-04-06 07:33:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -362,7 +362,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [
                 [
                     InlineKeyboardButton('🧲Group🧲', url=f'https://t.me/c_i_n_i_m_a_v_i_l_l_a'),
-                    InlineKeyboardButton('⭕️channel⭕️', url='https://t.me/+Tsu5PkbaWLNiYTZl')
+                    InlineKeyboardButton('⭕️channel⭕️', url='https://t.me/+2UJzU4_hibdhYzk1')
                 ]
                 
                 ]
@@ -414,7 +414,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [
                 [
                     InlineKeyboardButton('🧲Group🧲', url=f'https://t.me/c_i_n_i_m_a_v_i_l_l_a'),
-                    InlineKeyboardButton('⭕️channel⭕️', url='https://t.me/+Tsu5PkbaWLNiYTZl')
+                    InlineKeyboardButton('⭕️channel⭕️', url='https://t.me/+2UJzU4_hibdhYzk1')
                 ]
                 ]
         await query.answer()
@@ -892,14 +892,14 @@ async def auto_filter(client, msg, spoll=False):
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ Join Our Channel ⭕️",url="https://t.me/+Tsu5PkbaWLNiYTZl")]
+            [InlineKeyboardButton(text="⭕️ Join Our Channel ⭕️",url="https://t.me/+okpe9AyRx781OGFl")]
         )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="⭕️ Join Our Channel ⭕️",url="https://t.me/+Tsu5PkbaWLNiYTZl")]
+            [InlineKeyboardButton(text="⭕️ Join Our Channel ⭕️",url="https://t.me/+okpe9AyRx781OGFl")]
         )
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
